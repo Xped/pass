1899bcca1290146d7217f348e15033c9d28beba5
Xped <Xped@users.noreply.github.com>
2020-09-08 10:45:10 +0800

    Add files via upload

---
+++ static/js/href.js
@@ -0,0 +1,12 @@
+//地址必须为全路径带域名
+//老师
+window.fxsq='http://i.zuel.edu.cn/xsfw/sys/swmfxyqglapp/*default/index.do#/jzgfxsq';//返校申请地址
+//学生
+window.xsfxsq='http://i.zuel.edu.cn/xsfw/sys/swmfxyqglapp/*default/index.do#/';//返校申请地址
+window.engxsfxsq='http://i.zuel.edu.cn/xsfw/sys/swmfxyqglapp/*default/index.do#/lxsfxsq';//英文返校申请地址
+
+window.hbehbdmdz='';//黄鹤楼-链接湖北鄂汇办的码地址
+window.cxsq='http://i.zuel.edu.cn/xsfw/sys/swmwcsqglapp/*default/index.do';//出-链接学生出校申请的H5页面地址
+
+window.xwfkgl='http://i.zuel.edu.cn/qljfwapp/sys/lwWiseduOffcampusPerCheckin/*default/index.do';//访-链接吉化团队开发的校外访客管理H5页面地址
+window.jsdj='http://i.zuel.edu.cn/xsfw/sys/swmfxyqglapp/*default/index.do#/jsdt';//家-链接家属登记H5页面地址
